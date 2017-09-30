1bc5bd6726b10662922d97fe34e71dd0ebb94d78
HaoZJ <haozhaojun@eisti.eu>
2017-09-30 20:00:32 +0200

    删除冗余文件

--- ipynbs/unsupervised/codes/metric/__init__.py
+++
@@ -1,5 +0,0 @@
-from .chebyshev_distance import chebyshev_distance
-from .euclidean_distance import euclidean_distance
-from .hamming_distance import hamming_distance
-from .manhattan_distance import manhattan_distance
-from .minkowski_distance import minkowski_distance
